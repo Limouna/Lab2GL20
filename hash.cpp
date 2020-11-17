3768d4a902057272a52ad14787ef42677c82334c
Limouna <58744759+Limouna@users.noreply.github.com>
2020-11-17 19:14:43 +0100

    Prototype

--- hash.cpp
+++ hash.cpp
@@ -8,32 +8,100 @@ int hash1::HASH(string cle)
 {
     int index;
     int hashage = 0;
-
-    cout << cle.length();
-    //for sur le nombre de char de cle
     for(int i = 0; i< cle.length(); i++)
     {
-        // pour chaque char je reccupere le code ascii et je l'ajoute a la somme
         hashage = hashage + (int)cle[i];
-        cout << cle[i] << endl;
-        cout << hashage << endl;
     }
-
-    // modulo de la sommme des codes ascii sur tailleTableau
-
     index = hashage % tailleTableau;
-
-    //index = 0;
-    cout << index << endl;
 
     return index;
 }
 
-hash1::hash1(){
-     for (int i = 0; i<tailleTableau; i++){
-        HashTable[i] = new item;
-        HashTable[i]->nom = "vide";
-        HashTable[i]->tel = "vide";
-        HashTable[i]->next = NULL;
-     }
+void hash1::ajouterItem(string nom, string tel) {
+    int hashValue = HASH(nom);
+    auto& cell = table[hashValue];
+    auto bItr = begin(cell);
+    bool keyExists = false;
+    for (; bItr != end(cell); bItr++) {
+        if(bItr->first == nom) {
+            keyExists = true;
+            bItr -> second = tel;
+            cout << "[WARNING] Key exists. Value replaced" <<endl;
+            break;
+        }
+    }
+
+    if(!keyExists)
+        cell.emplace_back(nom, tel);
+    
+    return;
+}
+
+void hash1::afficherItem() {
+    for (int i{}; i<tailleTableau; i++) {
+        if (table[i].size() == 0) continue;
+        
+        auto bItr = table[i].begin();
+        for (; bItr != table[i].end(); bItr++) {
+            cout << "Nom: " << bItr->first << " et numero tel : " << bItr->second << endl;
+        }
+    }
+    return;
+}
+
+int hash1::nombreItems(int index) {
+    int nbreItem{};
+    for (int i{}; i<tailleTableau; i++) {
+        if (table[i].size() == 0) continue;
+        
+        auto bItr = table[i].begin();
+        for (; bItr != table[i].end(); bItr++) nbreItem++;
+    }
+    return nbreItem;
+}
+
+void hash1::afficherItemsDansIndex(int index) {
+    if (table[index].size() == 0) {
+        cout << "Aucun element dans cet indice" << endl;
+    }
+    else {
+        auto bItr = table[index].begin();
+        for (; bItr != table[index].end(); bItr++) {
+            cout << "Le nom est : " << bItr->first << " et le numero de tel est : " << bItr->second << endl;
+        }
+    }
+}
+
+void hash1::chercherItem(string nom) {
+    bool itemFound = false;
+    int index = HASH(nom);
+    auto bItr = table[index].begin();
+        for (; bItr != table[index].end(); bItr++) {
+            if (bItr->first == nom) {
+                cout << "Cetter personne exist dans la table" << endl;
+                itemFound = true;
+            }
+        }
+    if (!itemFound) cout << "Cetter personne n'existe pas dans la table" << endl;
+    return;
+}
+
+void hash1::supprimerItem(string nom) {
+    int hashValue = HASH(nom);
+    auto& cell = table[hashValue];
+    auto bItr = begin(cell);
+    bool keyExists = false;
+    for (; bItr != end(cell); bItr++) {
+        if(bItr->first == nom) {
+            keyExists = true;
+            bItr = cell.erase(bItr);
+            cout << "La personne " << nom << "a ete enlevee" << endl;
+            break;
+        }
+    }
+
+    if(!keyExists)
+        cout << "[WARNING] Cette personne n'a pas ete trouve" <<endl;
+
+    return;
 }
--- main.cpp
+++ main.cpp
@@ -7,9 +7,17 @@ using namespace std;
 
 int main()
 {
-    hash1 hashObj;
-    int index;
-    index = hashObj.HASH("Kerzazi");
+    hash1 HT;
+    
+    HT.ajouterItem("Hamid", "064132878");
+    HT.ajouterItem("Hamza", "074864287");
+    HT.ajouterItem("Ikram", "064513287");
+    HT.ajouterItem("Ayoub", "054961232");
+    HT.ajouterItem("Fatima", "067894315");
+    HT.ajouterItem("Ikhlass", "064295432");
+    HT.ajouterItem("Ayman", "Bruh 6");
+
+    HT.afficherItem();
 
 
     return 0;
